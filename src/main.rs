1a634f1bab9154b23d2d18c86b2c616c253c8f05
Joz <joz@spline.de>
2017-01-22 20:57:16 +0100

    U16

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
 use std::process::Command;
 
-fn get_width() -> u64{
+fn get_width() -> u16{
 	let columns = Command::new("tput").arg("cols").output().expect("failed to run tput");
 	let mut columns = String::from_utf8_lossy(&columns.stdout).into_owned();
 	let lastchar = columns.pop();
 	assert_eq!(lastchar, Some('\n'));
 
-	match columns.parse::<u64>() {
+	match columns.parse::<u16>() {
 		Ok(n) => n,
 		Err(_) => 80,
 	}
 }
 
-fn print_progress_bar(value: u64, max: u64) {
-	let width: u64 = get_width();
-	let inner_width: u64 = if width > 3 { width - 2 } else { 78 };
+fn print_progress_bar(value: usize, max: usize) {
+	let width = get_width() as usize;
+	let inner_width: usize = if width > 3 { width - 2 } else { 78 };
 	let mut line = String::new();
-	line.push('<');
+	line.push('[');
 
 	let label = format!("< {}% >", 100 * value / max);
 	let pos = value * inner_width / max;
@@ -29,8 +29,8 @@ fn print_progress_bar(value: u64, max: u64) {
 	for i in 1..remaining {
 		line.push('=');
 	}
+	line.push(']');
 	println!("written: {}", line.chars().count());
-	line.push('>');
 	print!("{}", line);
 }
 
