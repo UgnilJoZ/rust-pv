eb98a279403fa15ecb131698951a589fb0fe3333
Joz <joz@spline.de>
2017-01-23 01:27:46 +0100

    Give out the input on the output
    Still todo: Redirect ´tput rc´ to stderr

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use std::io;
 use std::io::prelude::*;
+use std::io::Write;
 use std::fs::File;
 
 use std::process::Command;
 use std::sync::{Arc, Mutex};
 use std::{thread,time,cmp};
-use std::sync::mpsc::channel;
 
-const zero: usize = 0;
+const ZERO: usize = 0;
 
 fn get_width() -> u16{
 	let columns = Command::new("tput").arg("cols").output().expect("failed to run tput");
@@ -26,7 +26,7 @@ fn save_cursor_pos() {
 }
 
 fn restore_cursor_pos() {
-	Command::new("tput").arg("rc").status().expect("failed to run tput");
+	Command::new("tput").arg("rc").stdout(io::stderr()).status().expect("failed to run tput");
 }
 
 fn print_progress_bar(value: usize, max: usize, width: usize) {
@@ -38,24 +38,25 @@ fn print_progress_bar(value: usize, max: usize, width: usize) {
 	let label_len = label.chars().count();
 	let pos = cmp::min(value * (inner_width - label_len) / max, inner_width - label_len);
 
-	for i in 1..pos {
+	for _ in 1..pos {
 		line.push('-');
 	}
 	line.push_str(label.as_str());
 	let remaining = (inner_width as u16) - (pos as u16) - (label_len as u16);
-	for i in 1..remaining {
+	for _ in 1..remaining {
 		line.push('=');
 	}
 	line.push(']');
-	print!("{}", line);
-	std::io::stdout().flush().unwrap();
+	write!(io::stderr(), "{}", line).unwrap();
 }
 
 fn main() {
-	let bytes_read  = Arc::new(Mutex::new(zero));
+	let bytes_read  = Arc::new(Mutex::new(ZERO));
 	let end_of_file = Arc::new(Mutex::new(false));
 	let file = io::stdin();
+	let mut output = io::stdout();
 	let bytes_max: usize = 1024;
+	save_cursor_pos();
 
 	{
 		let bytes_read = bytes_read.clone();
@@ -64,17 +65,15 @@ fn main() {
 			loop {
 				let bytes_read = *bytes_read.lock().unwrap();
 				if *end_of_file.lock().unwrap() {break;}
-				save_cursor_pos();
-				print_progress_bar(bytes_read, bytes_max, get_width() as usize);
-				std::io::stdout().flush().unwrap();
-				//println!("");
 				restore_cursor_pos();
+				print_progress_bar(bytes_read, bytes_max, get_width() as usize);
 				thread::sleep(time::Duration::from_millis(1000));
 			}
 		});
 	}
 
 	for b in file.bytes() {
+		output.write(&[b.unwrap()]).unwrap();
 		*bytes_read.lock().unwrap() += 1;
 	}
 	*end_of_file.lock().unwrap() = true; // signal to thread "finished!"
